48b8abdc5ea70e0115434170449d6c5d09f1ff46
Bibandeep Singh <biban354@gmail.com>
2021-02-02 18:13:32 +0530

    Practicing print() command after assigning the message to a variable.

---
+++ Famouse_Quote_2.py
@@ -0,0 +1,8 @@
+# Question: Repeat Exercise Famous_Quote, but this time, represent the famous person's name using a variable called
+#           famous_person. Then compose your message and represent it with a new variable called message. Print your
+#           message.
+
+# Solution:
+famous_person = 'Albert Einstein'
+message = f'{famous_person} once said, "A person who never made a mistake never tried anything new."'
+print(f"\n{message}")
