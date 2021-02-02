2007c3ca829e07f2a829af400e4f52f38d6f5390
Bibandeep Singh <biban354@gmail.com>
2021-02-02 18:05:11 +0530

    Revert "The very first python program of mine."
    This reverts commit 11202770

--- Famous_Quote.py
+++
@@ -1,6 +0,0 @@
-# Question: Find a quote from a famous person you admire. Print the quote and the name of its author.
-#           Your output should look something like the following, including the quotation marks:
-#           Albert Einstein once said, "A person who never made a mistake never tried anything new."
-
-# Solution:
-print('\nAlbert Einstein once said, "A person who never made a mistake never tried anything new."')
